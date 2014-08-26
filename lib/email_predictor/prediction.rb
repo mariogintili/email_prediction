9125c256d85f7b905af2a898b6300532aee98335
mariogintili <mariojgintili@gmail.com>
2014-08-26 11:48:26 +0100

    added prediction for first name and first inital

--- lib/email_predictor/prediction.rb
+++ lib/email_predictor/prediction.rb
@@ -7,5 +7,16 @@ module EmailPredictor
       @name   = name
       @domain = domain 
     end
+
+    def first_name_dot_last_name
+      address = [name.downcase.gsub(' ', '.'), domain].join("@")
+      @first_name_dot_last_name ||= Email.new address
+    end
+
+    def first_name_dot_last_initial
+      index   = name.rindex(' ') + 1
+      address = [name[0..index].downcase.gsub(' ', '.'), domain].join("@")
+      @first_name_dot_last_initial ||= Email.new address
+    end
   end
 end
--- spec/email_predictor/prediction_spec.rb
+++ spec/email_predictor/prediction_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 
 describe EmailPredictor::Prediction do
 
+  subject { Prediction.new(name, domain) }
+  
   context "#initialize" do
-
-    subject { Prediction.new(name, domain) }
 
     let(:domain) { "alphasights.com" }
     let(:name)   { "Peter Wong" }
@@ -12,6 +12,32 @@ describe EmailPredictor::Prediction do
     it "takes a name and a domain and sets it as an attribute" do
       expect(subject.name).to eq name
       expect(subject.domain).to eq domain
+    end
+  end
+
+  context "Predicting email, potential patterns" do
+
+    let(:domain) { "alphasights.com" }
+    let(:name)   { "Peter Wong" }
+
+    context "#first_name_dot_last_name" do
+
+      let(:predicted_address) { "peter.wong@alphasights.com" }
+
+      it "creates an email with the target's full name" do
+        expect(Email).to receive(:new).with(predicted_address)
+        subject.first_name_dot_last_name
+      end
+    end
+
+    context "#first_name_dot_last_initial" do
+
+      let(:predicted_address) { "peter.w@alphasights.com" }
+
+      it "#first_name_dot_last_initial" do
+        expect(Email).to receive(:new).with(predicted_address)
+        subject.first_name_dot_last_initial
+      end
     end
   end
 end
