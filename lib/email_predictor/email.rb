caff118f4c7f66bb6a8689a5aad95bea7623ca3b
mariogintili <mariojgintili@gmail.com>
2014-08-26 10:28:36 +0100

    added local and domain to email

--- lib/email_predictor/email.rb
+++ lib/email_predictor/email.rb
@@ -1,4 +1,28 @@
 module EmailPredictor
   class Email
+
+    attr_reader :address
+
+    def initialize(address)
+      @address = address
+    end
+
+    def domain
+      @domain ||= extract_domain
+    end
+
+    def local
+      @local ||= extract_local
+    end
+
+    private
+
+    def extract_domain
+      address.split(/@/).last
+    end
+
+    def extract_local
+      address.split(/@/).first
+    end
   end
 end
--- spec/email_predictor/email_spec.rb
+++ spec/email_predictor/email_spec.rb
@@ -1,4 +1,29 @@
 require "spec_helper"
 
 describe EmailPredictor::Email do
+
+  let(:address) { "john.ferguson@alphasights.com" }
+  
+  subject { Email.new(address) }
+  
+  context "#initialize" do
+
+    it "takes a string and sets it as an attribute" do
+      expect(subject.address).to eq address
+    end
+  end
+
+  context "#domain" do
+
+    it "returns the full domain" do
+      expect(subject.domain).to eq "alphasights.com"
+    end
+  end
+
+  context "#local" do
+
+    it "returns the local" do
+      expect(subject.local).to eq "john.ferguson"
+    end
+  end
 end
