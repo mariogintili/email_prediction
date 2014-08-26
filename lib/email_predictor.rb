6c0ce3b42ecab2a8707d4286eff6007123926cac
mariogintili <mariojgintili@gmail.com>
2014-08-26 13:17:18 +0100

    loaded Domain

--- lib/email_predictor.rb
+++ lib/email_predictor.rb
@@ -1,6 +1,7 @@
 module EmailPredictor
   autoload :DATA, "email_predictor/data"
   autoload :Name, "email_predictor/name"
+  autoload :Domain, "email_predictor/domain"
   autoload :Email, "email_predictor/email"
   autoload :Prediction, "email_predictor/prediction"
 end
